1fd0b319ed1c924e243b6d3c00642e9d0813e09c
victor viana <victor.deviana@outlook.com>
2024-11-24 04:06:22 -0300

    pianoActive

---
+++ engine/script.js
@@ -0,0 +1,21 @@
+const pianoKeys = document.querySelectorAll(".piano-keys .key");
+let audio = new Audio("./tunes/a.wav");
+const playTune = (key) => {
+  audio.src = `tunes/${key}.wav`;
+  audio.play();
+
+  const clickedKey = document.querySelector(`[data-key="${key}"]`);
+    clickedKey.classList.add("active");
+    setTimeout(() => {
+      clickedKey.classList.remove("active");
+  }, 150);
+};
+
+pianoKeys.forEach(key => {
+  console.log(key.dataset.key);
+  key.addEventListener("click", () => playTune(key.dataset.key));
+});
+
+document.addEventListener("keydown", (e) => {
+  playTune(e.key);
+});
