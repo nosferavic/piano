a7263ad5b2462ef179c4d427b5f768ebf0febaed
victor viana <victor.deviana@outlook.com>
2024-11-24 04:14:54 -0300

    volume

--- engine/script.js
+++ engine/script.js
@@ -1,21 +1,30 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
+let mapedKeys = [];
+const volumeSlider = document.querySelector(".volume-slider input");
 let audio = new Audio("./tunes/a.wav");
 const playTune = (key) => {
   audio.src = `tunes/${key}.wav`;
   audio.play();
-
   const clickedKey = document.querySelector(`[data-key="${key}"]`);
-    clickedKey.classList.add("active");
-    setTimeout(() => {
-      clickedKey.classList.remove("active");
+  clickedKey.classList.add("active");
+  setTimeout(() => {
+    clickedKey.classList.remove("active");
   }, 150);
 };
 
-pianoKeys.forEach(key => {
-  console.log(key.dataset.key);
+pianoKeys.forEach((key) => {
   key.addEventListener("click", () => playTune(key.dataset.key));
+  mapedKeys.push(key.dataset.key);
 });
 
 document.addEventListener("keydown", (e) => {
-  playTune(e.key);
+  if (mapedKeys.includes(e.key)) {
+    playTune(e.key);
+  }
 });
+
+const handleVolume = ((e) =>{
+  audio.volume = e.target.value
+})
+
+volumeSlider.addEventListener('input', handleVolume)
